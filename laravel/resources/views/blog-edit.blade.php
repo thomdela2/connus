e275bce79abe4fcb83d706c0bb24a3847b202cac
boldagencybe <thomas.delahaye@boldagency.be>
2020-06-07 17:13:25 +0200

    Blogs hardcoded

--- laravel/resources/views/blog-edit.blade.php
+++ laravel/resources/views/blog-edit.blade.php
@@ -10,6 +10,18 @@
 
     </head>
     <body>
-        Hier kun je de blogs editen
+        <header>
+            <h1>Header</h1>
+        </header>
+        <a class="add-btn" href="{{route('overviewblogs')}}">return</a>
+        <div class="blog-container">
+            <div class="blog">
+                <input type="text" name="title" id="input-title" placeholder="Titel">
+                <textarea name="content" id="" cols="30" rows="10" id="input-content">Inhoud</textarea>
+                <div class="edit-btn">
+                    <a class="btn" href="{{route('overviewblogs')}}">Add</a>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
--- laravel/resources/views/blog-overview.blade.php
+++ laravel/resources/views/blog-overview.blade.php
@@ -10,6 +10,19 @@
 
     </head>
     <body>
-        Hier kun je alle blogs zien
+        <header>
+            <h1>Header</h1>
+        </header>
+        <a class="add-btn" href="{{route('editblogs')}}">Add</a>
+        <div class="blog-container">
+            <div class="blog">
+                <h1>Lets try it out</h1>
+                <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet repellat sed fugit quibusdam qui unde neque iure commodi nihil nobis animi, dolore, incidunt cupiditate inventore numquam maxime quod porro. Adipisci.Fugit eos, sunt harum delectus saepe perspiciatis quae. Modi molestias tempora et ex, repudiandae dolor esse, unde facilis quibusdam aut provident, exercitationem eum! Eligendi vitae amet, magnam necessitatibus quis debitis.</2>
+                <div class="blog-btn">
+                    <a class="btn" href="{{route('editblogs')}}">Edit</a>
+                    <a class="btn">Delete</a>
+                </div>
+            </div>
+        </div>
     </body>
 </html>

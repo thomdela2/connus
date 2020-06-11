ea3d38fd669d1b9e8b1a611b469b613d321ebeaa
boldagencybe <thomas.delahaye@boldagency.be>
2020-06-11 21:20:54 +0200

    Last commit ot day

--- laravel/resources/views/blogs/blog.blade.php
+++ laravel/resources/views/blogs/blog.blade.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Blog | Connus</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Blog | Connus</title>
 
-    </head>
-    <body class="overview">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+</head>
+
+<body>
+    <div class="desktop">
         <div class="navigationcontainer">
-            <div class="blog-left">
-                <img class="logo--icon" href="{{ route('influencers') }}" src="images\connus.icon.blue.png" alt="logo">
+            <div class="leftnav">
+                <img class="logo--icon" href="{{ route('influencers') }}" src="images\connus.icon.white.png" alt="logo">
                 <a id="name" href="{{ route('influencers') }}">Connus</a>
                 <a href="{{ route('blogs') }}">Blogs</a>
                 <a href="{{ route('team') }}">Team</a>
-            <a href="{{ route('faq') }}">FAQ</a>
+                <a href="{{ route('faq') }}">FAQ</a>
             </div>
-            <div class="right">
+            <div class="rightnav">
                 {{-- <img src="" alt="stripe"> --}}
                 {{-- <p><a href="{{ route('influencers') }}">I am an influencer</a></p> --}}
             </div>
@@ -36,5 +39,7 @@
             @endforeach
 
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
--- laravel/resources/views/team.blade.php
+++ laravel/resources/views/team.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,22 +8,26 @@
     <title>Team | Connus</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body class="overview">
-    <div class="navigationcontainer">
-        <div class="leftnav">
-            <img class="logo--icon" href="{{ route('influencers') }}" src="images\connus.icon.white.png" alt="logo">
-            <a id="name" href="{{ route('influencers') }}">Connus</a>
-            <a href="{{ route('blogs') }}">Blogs</a>
-            <a href="{{ route('team') }}">Team</a>
-        <a href="{{ route('faq') }}">FAQ</a>
+
+<body>
+    <div class="desktop">
+        <div class="navigationcontainer">
+            <div class="leftnav">
+                <img class="logo--icon" href="{{ route('influencers') }}" src="images\connus.icon.white.png" alt="logo">
+                <a id="name" href="{{ route('influencers') }}">Connus</a>
+                <a href="{{ route('blogs') }}">Blogs</a>
+                <a href="{{ route('team') }}">Team</a>
+                <a href="{{ route('faq') }}">FAQ</a>
+            </div>
+            <div class="rightnav">
+                {{-- <img src="" alt="stripe"> --}}
+                {{-- <p><a href="{{ route('influencers') }}">I am an influencer</a></p> --}}
+            </div>
         </div>
-        <div class="rightnav">
-            {{-- <img src="" alt="stripe"> --}}
-            {{-- <p><a href="{{ route('influencers') }}">I am an influencer</a></p> --}}
+        <div class="background-images">
+            <img class="header--background" src="images\backgrounds\header.png" alt="header--background">
         </div>
-    </div>
-    <div class="background-images">
-        <img class="header--background" src="images\backgrounds\header.png" alt="header--background">
     </div>
 </body>
+
 </html>
